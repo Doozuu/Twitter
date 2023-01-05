80b099b47bb81b810cd5a89c3f9ea1b294287ffe
LJH <049494@ewhain.net>
2023-01-06 02:40:36 +0900

    Add Navigation, Log out, Redirect

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,17 +2,24 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Navigation from "./Navigation";
 // import EditProfile from "../routes/EditProfile";
-// import Profile from "../routes/Profile";
+import Profile from "../routes/Profile";
 
 function AppRouter({ isLoggedIn }) {
   return (
     <BrowserRouter>
+      {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
-          <Route path="/" element={<Home />}></Route>
+          <>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/profile" element={<Profile />}></Route>
+          </>
         ) : (
-          <Route path="/" element={<Auth />}></Route>
+          <>
+            <Route path="/" element={<Auth />}></Route>
+          </>
         )}
       </Routes>
     </BrowserRouter>
