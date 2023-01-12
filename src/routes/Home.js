a38508972796051a24c07f8d84200fbea8f76c7b
LJH <049494@ewhain.net>
2023-01-12 23:30:42 +0900

    Upload image to firestore

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
-import { useState } from "react";
-import { dbService } from "fbase";
+import { useState, useRef } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { ref, uploadString, getDownloadURL } from "@firebase/storage";
+import { dbService, storageService } from "fbase";
 import {
   addDoc,
   collection,
@@ -14,7 +16,7 @@ import Tweet from "components/Tweet";
 const Home = ({ userObj }) => {
   const [tweet, setTweet] = useState("");
   const [tweets, setTweets] = useState([]);
-  const [attachment, setAttachment] = useState();
+  const [attachment, setAttachment] = useState("");
   useEffect(() => {
     const q = query(collection(dbService, "tweets"), orderBy("createdAt"));
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
@@ -34,13 +36,28 @@ const Home = ({ userObj }) => {
 
   const onSubmit = async (event) => {
     event.preventDefault();
+    let attachmentUrl = "";
+    if (attachment !== "") {
+      //파일 경로 참조 만들기
+      const attachmentRef = ref(storageService, `${userObj.uid}/${uuidv4()}`);
+      //storage 참조 경로로 파일 업로드 하기
+      const response = await uploadString(
+        attachmentRef,
+        attachment,
+        "data_url"
+      );
+      //storage 참조 경로에 있는 파일의 URL을 다운로드해서 attachmentUrl 변수에 넣어서 업데이트
+      attachmentUrl = await getDownloadURL(response.ref);
+    }
     try {
       const docRef = await addDoc(collection(dbService, "tweets"), {
         text: tweet,
         createdAt: Date.now(),
         creatorId: userObj.uid,
+        attachmentUrl,
       });
       setTweet(""); // 입력창 비우기
+      setAttachment(""); // 파일 미리보기 이미지 src 비우기
       console.log("Document written with ID: ", docRef.id);
     } catch (error) {
       console.log("Error adding document", error);
@@ -67,7 +84,11 @@ const Home = ({ userObj }) => {
     };
     reader.readAsDataURL(theFile);
   };
-  const onClearAttachment = () => setAttachment(null);
+  const fileInput = useRef();
+  const onClearAttachment = () => {
+    setAttachment(null);
+    fileInput.current.value = ""; // clear하고 파일이름 남아있는거 지우기
+  };
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -78,7 +99,12 @@ const Home = ({ userObj }) => {
           maxLength={120}
           value={tweet}
         />
-        <input type="file" accept="image/*" onChange={onFileChange}></input>
+        <input
+          type="file"
+          accept="image/*"
+          onChange={onFileChange}
+          ref={fileInput}
+        ></input>
         <input type="submit" value="tweet" />
         {attachment && (
           <div>
